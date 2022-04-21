b448f010a9bf3c0dcb09b3f55e88f2d8398d0c9f
Muhammad Ibrahim <ibrahimraju347@gmail.com>
2022-04-21 15:15:09 +0500

    install react router 6 and also make the file of footer header resume etc

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
-
-
+import React from "react";
+import {Container,Grid} from '@material-ui/core'
+import Header from "./Components/Header/Header";
+import Profile from "./Components/Profile/Profile";
+import Footer from "./Components/Footer/Footer";
+import Portfolio from "./Pages/Porfolio/Portfolio";
+import Resume from "./Pages/Resume/Resume";
+import { Routes, Route } from "react-router-dom"
 function App() {
   return (
-    <div className="App">
-     Welcome to Portfolio
-    </div>
+    <Container>
+      <Grid Container>
+        <Grid item xs={12} sm={6} md={4} lg={3} style={{backgroundColor:"red"}}>
+            <Profile/>
+        </Grid>
+
+        <Grid  item xs={12} sm={6} md={8} lg={9} style={{backgroundColor:"green"}}>
+          <Header/>
+          <Routes>
+          <Route exact path="/portfolio" element={ <Portfolio/>} />
+          <Route exact path="/resume" element={ <Resume />} />
+          </Routes>
+          <Footer/>
+        </Grid>
+      </Grid>
+    </Container>
   );
 }
 
