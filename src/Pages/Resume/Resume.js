fc7d7e61f725f632a9201c084e8bb187c0d9c226
Muhammad Ibrahim <ibrahimraju347@gmail.com>
2022-04-26 15:03:02 +0500

    Contact form is completed

--- src/Pages/Resume/Resume.js
+++ src/Pages/Resume/Resume.js
@@ -1,13 +1,14 @@
 import React from 'react'
-import { Grid, Icon, Paper, Typography } from '@material-ui/core'
+import { Grid, Icon, Paper, TextField, Typography } from '@material-ui/core'
 import './resume.css'
 import resumeData from '../../utills/resumeData'
-import {CustomizeTimeline,CustomizeTimelineSeparator} from '../../Components/Timeline/CustomizeTimeline'
+import { CustomizeTimeline, CustomizeTimelineSeparator } from '../../Components/Timeline/CustomizeTimeline'
 import WorkIcon from '@material-ui/icons/Work';
 import { TimelineContent, TimelineDot, TimelineItem } from '@material-ui/lab'
 import SchoolIcon from '@material-ui/icons/School';
+import CustomButton from '../../Components/Button/CustomButton'
 
-const Resume=()=>{
+const Resume = () => {
   return (
     <>
       {/* About me */}
@@ -32,32 +33,32 @@ const Resume=()=>{
           <Grid container className='resumeTimeline'>
             {/* Experience*/}
             <Grid item sm={12} md={6}>
-              <CustomizeTimeline title='Work Experience' icon={<WorkIcon/>} >
-              {resumeData.experience.map((exprience)=>(
-                <TimelineItem >
-                    <CustomizeTimelineSeparator/>
+              <CustomizeTimeline title='Work Experience' icon={<WorkIcon />} >
+                {resumeData.experience.map((exprience) => (
+                  <TimelineItem >
+                    <CustomizeTimelineSeparator />
                     <TimelineContent className='timeline_content'>
                       <Typography className='timeline_title'>{exprience.title}</Typography>
                       <Typography variant='caption' className='timeline_date'>{exprience.date}</Typography>
                       <Typography variant="body2" className='timeline_description'>{exprience.description}</Typography>
                     </TimelineContent>
-                </TimelineItem>
-              ))}
+                  </TimelineItem>
+                ))}
               </CustomizeTimeline>
             </Grid>
             {/* Education */}
             <Grid item sm={12} md={6}>
               <CustomizeTimeline title='Education Detail' icon={<SchoolIcon />} >
-              {resumeData.education.map((education)=>(
-                <TimelineItem >
-                    <CustomizeTimelineSeparator/>
+                {resumeData.education.map((education) => (
+                  <TimelineItem >
+                    <CustomizeTimelineSeparator />
                     <TimelineContent className='timeline_content'>
                       <Typography className='timeline_title'>{education.title}</Typography>
                       <Typography variant='caption' className='timeline_date'>{education.date}</Typography>
                       <Typography variant="body2" className='timeline_description'>{education.description}</Typography>
                     </TimelineContent>
-                </TimelineItem>
-              ))}
+                  </TimelineItem>
+                ))}
               </CustomizeTimeline>
             </Grid>
           </Grid>
@@ -66,21 +67,21 @@ const Resume=()=>{
 
       {/* Services */}
       <Grid container className='section  pb_45'>
-      <Grid item className='section_title mb_30'>
+        <Grid item className='section_title mb_30'>
           <span></span>
           <h6 className='section_title_text'>Services</h6>
         </Grid>
 
         <Grid item xs={12} >
           <Grid container spacing={3} justify='space-around'>
-            {resumeData.services.map((services)=>(
+            {resumeData.services.map((services) => (
               <Grid item xs={12} sm={6} md={3}>
                 <div className='services'>
                   <Icon className='services_icon'>{services.icon}</Icon>
                   <Typography className='services_title' variant='h6'>{services.title}</Typography>
-                  <Typography className='services_description'variant='body2' >{services.description}</Typography>
+                  <Typography className='services_description' variant='body2' >{services.description}</Typography>
                 </div>
-                </Grid>
+              </Grid>
             ))}
           </Grid>
         </Grid>
@@ -90,30 +91,87 @@ const Resume=()=>{
       <Grid container className='section graybg pb_45 p_50' >
         <Grid item xs={12}>
           <Grid container spacing={3} justify="space-between" >
-          {resumeData.skill.map((skill)=>(
-        <Grid item xs={12} sm={6} md={3} >
-          <Paper elevation={0} className='skill'>
-            <Typography variant='h6' className='skill_title'>{skill.title}</Typography>
-            {skill.describe.map((des)=>(
-              <Typography variant='body2' className='skill_description'>
-                <TimelineDot className='timeline_dot'/>
-                {des}
-              </Typography>
+            {resumeData.skill.map((skill) => (
+              <Grid item xs={12} sm={6} md={3} >
+                <Paper elevation={0} className='skill'>
+                  <Typography variant='h6' className='skill_title'>{skill.title}</Typography>
+                  {skill.describe.map((des) => (
+                    <Typography variant='body2' className='skill_description'>
+                      <TimelineDot className='timeline_dot' />
+                      {des}
+                    </Typography>
+                  ))}
+                </Paper>
+              </Grid>
             ))}
-          </Paper>
-        </Grid>
-      ))}
           </Grid>
         </Grid>
-     
+
       </Grid>
 
       {/* Contact */}
-      <Grid container>
+      <Grid container className="section pb_45 p_50" spacing={6}>
+        <Grid item xs={12} lg={7}>
+          <Grid container >
+            <Grid item className='section_title mb_30'>
+              <span></span>
+              <h6 className='section_title_text'>Contact Us</h6>
+            </Grid>
+          </Grid>
+          
 
-      </Grid>
-    </>
-  )
+          <Grid item xs={12} >
+            <Grid container spacing={3} >
+              <Grid item xs={12} sm={6}>
+                <TextField fullWidth name="name" label="Name"/>
+              </Grid>
+              <Grid item xs={12} sm={6}>
+                <TextField fullWidth  name="email" label="E-mail"/>
+              </Grid>
+              <Grid item xs={12} >
+                <TextField fullWidth name="message" label="Message" multiline rows={4}/>
+              </Grid>
+
+              <Grid item xs={12}>
+                <CustomButton text='Submit'/>
+              </Grid>
+            </Grid>
+          </Grid>
+          </Grid>
+          {/* contact information */}
+          <Grid item xs={12} lg={5}>
+          <Grid container >
+            <Grid item className='section_title mb_30'>
+              <span></span>
+              <h6 className='section_title_text'>Contact Information</h6>
+            </Grid>
+            <Grid item xs={12}>
+              <Grid container>
+                <Grid item>
+                <Typography className='contactinfo_item'> <span>  Address :</span> {resumeData.address}</Typography>
+                <Typography className='contactinfo_item'> <span>  Phone :</span> {resumeData.phone}</Typography>
+                <Typography className='contactinfo_item'> <span>  E-mail :</span> {resumeData.email}</Typography>
+                </Grid>
+              </Grid>
+            </Grid>
+            <Grid item xs={12}>
+              <Grid container className="contactinfo_socialContainer">
+                {Object.keys(resumeData.social).map((key)=>(
+                  <Grid item className='contactinfo_social'>
+                    <a href={resumeData.social[key].link}>
+                      {resumeData.social[key].icon}
+                    </a>
+                  </Grid>
+                ))}
+              </Grid>
+            </Grid>
+          </Grid>
+          </Grid>
+        </Grid>
+
+
+      </>
+      )
 }
 
-export default Resume
+      export default Resume
